6c9be2ac82de28ec6b04cf0dbace51c2b15f82b3
Victor Campolina <victor.cs2001.vc@gmail.com>
2021-11-05 15:42:38 -0300

    Aula-91 - Navegação Stack #06

--- src/routes/Stack.js
+++ src/routes/Stack.js
@@ -20,15 +20,16 @@ export default props => (
         </Stack.Screen>
         <Stack.Screen name="TelaB">
             {props => (
-                <PassoStack {...props} voltar  avancar="TelaC">
+                <PassoStack {...props} voltar  avancar="TelaC"
+                    avancarParams={{ numero: 1007 }}>
                     <TelaB />
                 </PassoStack>
             )}
         </Stack.Screen>
         <Stack.Screen name="TelaC">
             {props => (
-                <PassoStack {...props}voltar avancar="TelaC">
-                    <TelaC />
+                <PassoStack {...props} voltar avancar="TelaC">
+                    <TelaC {...props}/>
                 </PassoStack>
             )}
         </Stack.Screen>
