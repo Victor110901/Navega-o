bb83cbd85eb3aee511946cd116e4693b928be14d
Victor Campolina <victor.cs2001.vc@gmail.com>
2021-11-05 15:04:53 -0300

    Aula-87 - Navegação Stack #02

--- src/components/TextoCentral.js
+++ src/components/TextoCentral.js
@@ -4,8 +4,8 @@ import { View, Text } from 'react-native'
 export default props => (
     <View style={{
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
+        justifyContent: "center",
+        alignItems: "center",
         backgroundColor: props.corFundo || '#000'
     }}>
         <Text style={{
